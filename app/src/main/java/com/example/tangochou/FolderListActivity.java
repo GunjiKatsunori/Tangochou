fbf33f69d924518ab42ff79897f60d7e2ba14961
KatsunoriGunji <l.hvokj@gmail.com>
2020-11-04 17:30:50 +0900

    activity, adapterをViewの処理の記述に限定

--- app/src/main/java/com/example/tangochou/FolderListActivity.java
+++ app/src/main/java/com/example/tangochou/FolderListActivity.java
@@ -1,12 +1,12 @@
 package com.example.tangochou;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.model.FolderModel;
 
 import java.util.ArrayList;
 
@@ -15,34 +15,46 @@ import java.util.ArrayList;
  */
 public class FolderListActivity extends AppCompatActivity {
     // 表示するデータのリスト
-    private static ArrayList<String> folders = new ArrayList<>();
+    private static ArrayList<FolderModel> folders = new ArrayList<>();
 
     FolderListAdapter adapter;
-    FolderOpenHelper helper;
-    SQLiteDatabase db;
+    FolderListPresenter folderListPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.folder_list);
 
-        // DBからデータを呼び出す
-        helper = new FolderOpenHelper(getApplicationContext());
-        db = helper.getReadableDatabase();
-        Cursor cursor = db.query("folder", new String[]{"name"}, null, null, null, null, null);
-        cursor.moveToFirst();
-        for (int i=0; i<cursor.getCount(); i++) {
-            folders.add(cursor.getString(0));
-            cursor.moveToNext();
-        }
-        cursor.close();
+        // リスト表示のためのデータを呼び出す
+        folderListPresenter = new FolderListPresenter(this);
+        folders = folderListPresenter.openFolderList(null);
 
         // フォルダリストを表示する
         RecyclerView folderRecyclerView = findViewById(R.id.folder_recycler_view);
         folderRecyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         folderRecyclerView.setLayoutManager(layoutManager);
-        adapter = new FolderListAdapter(folders);
+        adapter = new FolderListAdapter(this, folders);
         folderRecyclerView.setAdapter(adapter);
+
+        /*
+        ItemTouchHelper ith = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT) {
+                    // 長押しでフォルダ名変更
+                    @Override
+                    public boolean onLongClick(RecyclerView recyclerView) {
+                        // positionを取得
+                        final int position = holder.getAdapterPosition();
+                        String selectedText = dataset.get(position);
+
+
+
+                        return true;
+                    }
+
+                });
+
+        ith.attachToRecyclerView(folderRecyclerView);
+        */
     }
 }
--- app/src/main/java/com/example/tangochou/FolderListAdapter.java
+++ app/src/main/java/com/example/tangochou/FolderListAdapter.java
@@ -1,20 +1,22 @@
 package com.example.tangochou;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.model.FolderModel;
+
 import java.util.ArrayList;
 
 public class FolderListAdapter extends RecyclerView.Adapter<FolderListAdapter.ViewHolder> {
-    // フォルダー一覧で表示するフォルダのリストのデータ
-    private ArrayList<String> dataset = new ArrayList<String>();     // あとでデータ型修正
+    private Context context;
+    private ArrayList<FolderModel> dataset;
 
     // ホルダーの定義
     static class ViewHolder extends RecyclerView.ViewHolder {
@@ -29,14 +31,15 @@ public class FolderListAdapter extends RecyclerView.Adapter<FolderListAdapter.Vi
     }
 
     // コンストラクター
-    FolderListAdapter(ArrayList<String> dataset) {  // あとでデータ型修正
+    FolderListAdapter(Context context, ArrayList<FolderModel> dataset) {
+        this.context = context;
         this.dataset = dataset;
     }
 
     @Override
     @NonNull
     public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int ViewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.folder_item, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.folder_item, parent, false);
         final ViewHolder holder = new ViewHolder(view);
 
         return holder;
@@ -45,7 +48,7 @@ public class FolderListAdapter extends RecyclerView.Adapter<FolderListAdapter.Vi
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // 並び順を更新
-        holder.folderName.setText(dataset.get(position));
+        holder.folderName.setText(dataset.get(position).getName());
     }
 
     @Override
