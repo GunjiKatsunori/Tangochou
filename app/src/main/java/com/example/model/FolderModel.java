09e524669d8ad25b19cccfdc08d29977f1a9bdef
KatsunoriGunji <l.hvokj@gmail.com>
2020-11-13 16:31:44 +0900

    Folder, Series, Cardを共通化するためのインターフェース導入

--- app/src/main/java/com/example/model/FolderModel.java
+++ app/src/main/java/com/example/model/FolderModel.java
@@ -1,13 +1,28 @@
 package com.example.model;
 
-public class FolderModel {
-    // Folderのカラム
+/**
+ * Folderテーブルのデータを格納するモデルクラス
+ * @author 郡司克徳
+ * @version 1.0.0
+ */
+public class FolderModel implements IFile {
+    /**
+     * Folderテーブルのカラム
+     */
     private Integer id;
     private String path;
     private String name;
     private String directory;
     private Integer parent_id;
 
+    /**
+     * コンストラクタ
+     * @param id
+     * @param path
+     * @param name
+     * @param directory
+     * @param parent_id
+     */
     public FolderModel(Integer id, String path, String name, String directory, Integer parent_id) {
         this.id = id;
         this.path = path;
@@ -16,22 +31,46 @@ public class FolderModel {
         this. parent_id = parent_id;
     }
 
+    /**
+     * IDを取得する
+     * @return ID
+     */
+    @Override
     public Integer getId() {
         return id;
     }
 
+    /**
+     * パスを取得する
+     * @return パス
+     */
     public String getPath() {
         return path;
     }
 
+    /**
+     * フォルダ名を取得する
+     * @return フォルダ名
+     */
+    @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * 所属するディレクトリを取得する
+     * @return 所属するディレクトリ
+     */
+    @Override
     public String getDirectory() {
         return directory;
     }
 
+    /**
+     * 所属するフォルダのIDを取得する
+     * @return　所属するフォルダのID
+     */
+    @Override
     public Integer getParentId() {
         return parent_id;
     }
